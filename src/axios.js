31984b7b539b9cf517ae407d69a81023344cfc5d
Diyar <diyar@Diyars-MacBook-Pro-M1.local>
2022-01-21 00:06:58 +0300

    adding files

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/",
-  // https://questions-server.herokuapp.com/
+  baseURL: "https://questions-server.herokuapp.com/",
+  // http://localhost:8000/
 });
 export default instance;
